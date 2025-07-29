7d6fcda66595a7df95e1c45cff31ad3ed7cf77b3
Lucas <lucasdm534@gmail.com>
2025-07-28 21:22:14 -0300

    second commit

--- js/script.js
+++ js/script.js
@@ -1,7 +1,5 @@
 const thumbnails = document.querySelectorAll('.imgs-galeria img');
 const imagemPrincipal = document.getElementById('imagem-principal');
-const tituloPrincipal = document.getElementById('titulo-principal');
-const descricaoPrincipal = document.getElementById('descricao-principal');
 
 thumbnails.forEach(img => {
     img.addEventListener('click', () => {
@@ -22,3 +20,42 @@ thumbnails.forEach(img => {
 if(thumbnails.length > 0){
     thumbnails[0].classList.add('ativo');
 }
+
+// Configurações do botão do WhatsApp fixo no canto inferior
+window.addEventListener('scroll', function(){
+    const btn = document.getElementById('whatsapp-btn');
+    const scrollY = window.scrollY;
+
+    // Define o ponto onde o botão aparece
+    if(scrollY > 200){
+        btn.classList.add('show');
+    } else{
+        btn.classList.remove('show');
+    }
+});
+
+// Animação das sections
+function animarAoScroll() {
+    const elementos = document.querySelectorAll('[data-anima]');
+    const windowTop = window.scrollY + window.innerHeight * 0.85;
+
+    elementos.forEach(el => {
+        if (el.offsetTop < windowTop) {
+            el.classList.add('animado');
+        }
+    });
+}
+
+window.addEventListener('scroll', animarAoScroll);
+window.addEventListener('load', animarAoScroll);
+
+// Fecha o menu quando um link do NAV é clicado
+document.querySelectorAll('.navbar-nav .nav-link').forEach(link => {
+    link.addEventListener('click', () => {
+        const navbarCollapse = document.querySelector('.navbar-collapse');
+        const bsCollapse = new bootstrap.Collapse(navbarCollapse, {
+            toggle: false
+        });
+        bsCollapse.hide();
+    });
+});
